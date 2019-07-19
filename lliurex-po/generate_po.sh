1e38a89c64dc1bd262d1604589eeb4c93e92ca7f
jrpelegrina <juapesai@hotmail.com>
2019-07-19 09:10:57 +0200

    Added desktop launcher to check group user

--- lliurex-po/generate_po.sh
+++ lliurex-po/generate_po.sh
@@ -3,3 +3,4 @@
 xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/lliurex-up.py -o ./lliurex-up/lliurex-up.pot
 xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/rsrc/lliurex-up.ui  -o ./lliurex-up/lliurex-up.pot
 xgettext --join-existing -L python ../lliurex-up-indicator/usr/bin/lliurex-up-indicator -o ./lliurex-up/lliurex-up.pot
+xgettext --join-existing -L python ../lliurex-up/usr/bin/lliurex-up-desktop-launcher.py -o ./lliurex-up/lliurex-up.pot
