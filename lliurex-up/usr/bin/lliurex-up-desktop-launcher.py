ac5184ea7dabc9e963505c5848605dece2d2903d
Raul Rodrigo Segura <raurodse@gmail.com>
2020-10-06 10:04:43 +0200

    add screensaver control

--- lliurex-up/usr/bin/lliurex-up-desktop-launcher.py
+++ lliurex-up/usr/bin/lliurex-up-desktop-launcher.py
@@ -5,7 +5,7 @@ import os
 import subprocess
 import sys
 import grp
-
+import lliurex.screensaver
 import gettext
 gettext.textdomain('lliurex-up')
 _= gettext.gettext
@@ -34,15 +34,18 @@ class LlxUpCheckRoot():
 						
 		if group_found:		
 
+			screensaver_inhibitor = lliurex.screensaver.InhibitScreensaver()
+			
+			screensaver_inhibitor.inHibit()
 			cmd='pkexec lliurex-up'
+			os.system(cmd)
+			screensaver_inhibitor.unInhibit()
 		else:
 			text=_("You need administration privileges to run this application.")
 			cmd='kdialog --icon lliurex-up --title "Lliurex-Up" --passivepopup \
 			"%s" 5'%text
+			os.system(cmd)
 					
-
-		os.system(cmd)
-
 	#def check_root
 	
 #def LlxUpCheckRoot
