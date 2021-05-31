e2f718e1b8d5b30e61effa4596b525dedddf1cc3
Juan Ramon Pelegrina <juapesai@hotmail.com>
2021-05-31 10:11:56 +0200

    Clean some code and improved feedback

--- lliurex-up/usr/share/lliurex-up/PreferencesBox.py
+++ lliurex-up/usr/share/lliurex-up/PreferencesBox.py
@@ -31,10 +31,14 @@ class PreferencesBox(Gtk.VBox):
 
 		ui_path=self.core.ui_path
 		builder.add_from_file(ui_path)
+		self.css_file=self.core.rsrc_dir+"lliurex-up.css"
 
 	
 		self.main_box=builder.get_object("preferences_box")
 		self.preferences_label=builder.get_object("preferences_label")
+		self.confirmation_box=builder.get_object("confirmation_box")
+		self.confirmation_img_ok=builder.get_object("confirmation_img_ok")
+		self.confirmation_label=builder.get_object("confirmation_label")
 		self.systray_box=builder.get_object("systray_box")
 		self.systray_label=builder.get_object("systray_label")
 		self.systray_switch=builder.get_object("systray_switch")
@@ -46,8 +50,21 @@ class PreferencesBox(Gtk.VBox):
 		else:
 			self.systray_switch.set_active(False)
 
-				
+		self.set_css_info()	
 	#def __init__
+
+	def set_css_info(self):
+		
+		self.style_provider=Gtk.CssProvider()
+
+		f=Gio.File.new_for_path(self.css_file)
+		self.style_provider.load_from_file(f)
+
+		Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(),self.style_provider,Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+		self.confirmation_box.set_name("SUCCESS_BOX")
+			
+	#def set-css_info
+
 	
 	def show_systray_switch (self):
 
@@ -64,7 +81,8 @@ class PreferencesBox(Gtk.VBox):
 				if self.core.llxUpConnect.search_meta('client'):	
 					systray=False
 
-			if systray: 		
+			if systray: 
+				self.confirmation_box.hide()		
 				self.systray_box.show()
 
 		except Exception as e:	
@@ -85,7 +103,7 @@ class PreferencesBox(Gtk.VBox):
 				f=open(DISABLE_INDICATOR_TOKEN,'w')
 				f.close()
 
-
+		self.confirmation_box.show()
 	#def config_systray		
 	
 		
