dc367bbc3c0d8cd57b44aacc16fb15cd654bf85b
Juan Pelegrina <juapesai@hotmail.com>
2021-11-17 13:29:30 +0100

    Updated po file to new gui

--- lliurex-po/generate_po.sh
+++ lliurex-po/generate_po.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/lliurex-up.py -o ./lliurex-up/lliurex-up.pot
+xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/MainWindow.py -o ./lliurex-up/lliurex-up.pot
+xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/LoadBox.py -o ./lliurex-up/lliurex-up.pot
+xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/InformationBox.py -o ./lliurex-up/lliurex-up.pot
+xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/OptionsBox.py -o ./lliurex-up/lliurex-up.pot
+xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/PackagesBox.py -o ./lliurex-up/lliurex-up.pot
+xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/TerminalBox.py -o ./lliurex-up/lliurex-up.pot
+xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/PreferencesBox.py -o ./lliurex-up/lliurex-up.pot
 xgettext --join-existing ../lliurex-up/usr/share/lliurex-up/rsrc/lliurex-up.ui  -o ./lliurex-up/lliurex-up.pot
 xgettext --join-existing -L python ../lliurex-up-indicator/usr/bin/lliurex-up-indicator -o ./lliurex-up/lliurex-up.pot
 xgettext --join-existing -L python ../lliurex-up/usr/bin/lliurex-up-desktop-launcher.py -o ./lliurex-up/lliurex-up.pot
